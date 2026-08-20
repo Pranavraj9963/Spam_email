5b91d4f8ea135b03add8c3ecea7b3d03b6bbec58
Pranavraj9963 <57pranavraj11scia@gmail.com>
2026-08-20 20:38:12 +0530

    utils-2

---
+++ utils/preprocessing.py
@@ -0,0 +1,28 @@
+import re
+
+import nltk
+
+from nltk.corpus import stopwords
+
+from nltk.stem import WordNetLemmatizer
+
+nltk.download("stopwords")
+nltk.download("wordnet")
+
+stop_words = set(stopwords.words("english"))
+
+lemmatizer = WordNetLemmatizer()
+
+def preprocess_text(text):
+
+    text = text.lower()
+
+    text = re.sub(r"[^a-zA-Z]", " ", text)
+
+    words = text.split()
+
+    words = [lemmatizer.lemmatize(word)
+             for word in words
+             if word not in stop_words]
+
+    return " ".join(words)
